d2985f768f2db86d4d313fe5fe1b653363b91b80
Vedarth007 <103764711+Vedarth007@users.noreply.github.com>
2022-10-06 11:11:13 +0530

    Add files via upload

---
+++ mat.py
@@ -0,0 +1,14 @@
+"""Vedarth khandve"""
+
+m=[]
+r=int(input("Enter teh no. of rows"))
+c=int(input("Enter the no of colums"))
+
+print("matrix is in row major order")
+for i in range (r)
+	a=[]
+	for j in range (c)
+	a.append(int(input("enter the elements of matrix:",i,j)));
+m.append(a)
+print(m)
+	
